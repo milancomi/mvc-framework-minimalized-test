83718c86fac306b65bd9e7d45b3f124e59aabbe6
milancomi <38693770+milancomi@users.noreply.github.com>
2020-04-06 18:46:36 +0200

    kl;k

--- views/layout/header.php
+++ views/layout/header.php
@@ -133,7 +133,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="#" class="brand-link">
       <span class="brand-text font-weight-light">Php Framework</span>
     </a>
 
